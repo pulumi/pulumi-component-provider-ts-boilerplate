e9f4e4b60795a4d750fd7360d31a4d9686755b1b
Mikhail Shilkov <github@mikhail.io>
2022-03-17 11:43:45 +0100

    Remove node example

--- examples/simple/index.ts
+++
@@ -1,8 +0,0 @@
-import * as xyz from "@pulumi/xyz";
-
-const page = new xyz.StaticPage("page", {
-    indexContent: "<html><body><p>Hello world!</p></body></html>",
-});
-
-export const bucket = page.bucket;
-export const url = page.websiteUrl;
